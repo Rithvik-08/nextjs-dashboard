ee2ef5f51dd32aa272904092382700babe280a5a
Rithvik-08 <96409244+Rithvik-08@users.noreply.github.com>
2024-11-19 10:21:16 +0530

    commit5

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -88,7 +88,6 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 export async function updateInvoice(
   id: string,
-  prevState: State,
   formData: FormData,
 ) {
   const validatedFields = UpdateInvoice.safeParse({
